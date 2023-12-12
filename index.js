c2e555e55802511bb2a3e5bbf38d48160426e60e
AbdullahDaniyal <abdullahdaniyal67@gmail.com>
2023-12-12 14:20:12 +0500

    Add some modifications to Index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -16,26 +15,3 @@ mongoose.connect('mongodb+srv://abdullahdaniyal:superflies1234@cluster0.s5b7diq.
 
 const port = 3000;
 app.listen(port, () => console.log(`Server listening on port http://localhost:${port}`));
-=======
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-app.listen(process.env.PORT || 3000, () => {
-  "Server is running...";
-});
-
-mongoose
-  .connect(process.env.DB_STRING)
-  .then(() => {
-    console.log("Connected to mongodb");
-  })
-  .catch(err => {
-    console.log(err);
-  });
->>>>>>> d4c72f9952e9c525ade5ab3a8ccfc01ac3f02095
