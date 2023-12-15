4a74bdaa56336745cbe8ab0a630763b74377b939
AbdullahDaniyal <abdullahdaniyal67@gmail.com>
2023-12-16 01:12:49 +0500

    Forgot password function implemented from Traveller Panel

--- TravellerPanel/Controller/userController.js
+++ TravellerPanel/Controller/userController.js
@@ -1,7 +1,7 @@
-
 const User = require('../Schema/userProfile'); // Assuming you have a User.js in your models directory
 const jwt = require('jsonwebtoken');
 const SECRET_KEY = "mysecretkey";
+const nodemailer = require('nodemailer');
 
 const signup_user = async (req, res) => {
     const { name, email, password, CNIC, contact, preferences } = req.body;
@@ -18,7 +18,7 @@ const login_user = async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await User.findOne({ email });
-        if (user && password === user.password && !user.disabled) {
+        if (user && password === user.password) {
             const token = jwt.sign({ id: user._id }, SECRET_KEY, { expiresIn: '1d' });
             res.cookie('auth_token', token);
             res.status(200).send(`Login Successful ${user.name}`);
@@ -30,4 +30,38 @@ const login_user = async (req, res) => {
     }
 };
 
-module.exports = { signup_user, login_user };
+const forgot_password = async (req, res) => {
+    const { email, CNIC } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (user && CNIC === user.CNIC) {
+            let testAccount = await nodemailer.createTestAccount();
+
+            const transporter = nodemailer.createTransport({
+                host: 'smtp.ethereal.email',
+                port: 587,
+                auth: {
+                    user: 'akeem63@ethereal.email',
+                    pass: 'b3AG6n6aSKXhZZDASk'
+                }
+            });
+
+            let info = await transporter.sendMail({
+                from: '"Abdullah Daniyal" <abdullah@voyagevista.com>', // sender address
+                to: user.email, // list of receivers
+                subject: "Password Recovered", // Subject line
+                text: `Hey ${user.name}, your password has been recovered ${user.password}`, // plain text body
+                html: `Hey ${user.name}, your password has been recovered. Your password was <b>${user.password}</b>`, // html body
+            });
+
+            console.log("Message sent: %s", info.messageId);
+            res.status(200).send(`Password sent to ${user.email}`);
+        } else {
+            res.status(401).send('Invalid email or password');
+        }
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+};
+
+module.exports = { signup_user, login_user, forgot_password };
--- TravellerPanel/Routes/userRoutes.js
+++ TravellerPanel/Routes/userRoutes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../Controller/userController');
 const { authenticate_user } = require('../Middleware/authMiddleware');
-const { signup_user, login_user } = require('../Controller/userController');
+const { signup_user, login_user, forgot_password } = require('../Controller/userController');
 
 // Route to handle user signup
 router.post('/signup_user', userController.signup_user);
 
 // Route to handle user login
 router.post('/login_user', userController.login_user);
+
+router.post('/forgot_password', userController.forgot_password);
 
 module.exports = router;
