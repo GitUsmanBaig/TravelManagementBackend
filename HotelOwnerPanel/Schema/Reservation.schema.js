9019263fe116520544d56ebcd53f19743aab9155
saimalam843 <saimalam843@gmail.com>
2023-12-18 17:31:30 +0500

    Hotel Owner Panel schemas implemented

---
+++ HotelOwnerPanel/Schema/Reservation.schema.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const reservationSchema = new mongoose.Schema({
+  hotel: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Hotel',
+    required: true
+  },
+  guest: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Customer', 
+    required: true
+  },
+  checkInDate: {
+    type: Date,
+    required: true
+  },
+  checkOutDate: {
+    type: Date,
+    required: true
+  },
+  numberOfGuests: {
+    type: Number,
+    required: true
+  },
+  roomsBooked: {
+    type: Number,
+    required: true
+  },
+  specialRequests: String, // for any special requests from the guest
+  totalAmount: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'confirmed', 'declined', 'cancelled'],
+    default: 'pending'
+  },
+  bookingDate: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Reservation = mongoose.model('Reservation', reservationSchema);
+
+module.exports = Reservation;
---
+++ HotelOwnerPanel/Schema/Review.schema.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  hotel: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Hotel',
+    required: true
+  },
+  guest: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Customer', 
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5
+  },
+  comment: {
+    type: String,
+    required: 'Please provide a review comment'
+  },
+  reviewDate: {
+    type: Date,
+    default: Date.now
+  },
+  response: {
+    type: String // for the hotel owner's response to the review
+  }
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
