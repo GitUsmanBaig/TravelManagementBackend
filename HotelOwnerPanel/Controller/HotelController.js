8354aaffc9dc550a89412b17a9e748e9fe5a2a36
saimalam843 <saimalam843@gmail.com>
2023-12-19 10:48:16 +0500

    Hotel Routes Implemented (Excluding the Images Functionality)

--- HotelOwnerPanel/Controller/HotelController.js
+++ HotelOwnerPanel/Controller/HotelController.js
@@ -1,1 +1,58 @@
-console.log('Saim')
+const Hotel = require('../Schema/Hotel.schema');
+
+const HotelController = {
+    listProperties: async (req, res) => {
+        try {
+            const hotels = await Hotel.find({ owner: req.hotelOwner._id });
+            res.json(hotels);
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    },
+
+    getPropertyDetails: async (req, res) => {
+        try {
+            const hotel = await Hotel.findOne({ _id: req.params.id, owner: req.hotelOwner._id });
+            if (!hotel) return res.status(404).send('Hotel not found');
+            res.json(hotel);
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    },
+
+    createProperty: async (req, res) => {
+        try {
+            const newHotel = new Hotel({ ...req.body, owner: req.hotelOwner._id });
+            await newHotel.save();
+            res.status(201).send(newHotel);
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    },
+
+    updatePropertyDetails: async (req, res) => {
+        try {
+            const updatedHotel = await Hotel.findOneAndUpdate(
+                { _id: req.params.id, owner: req.hotelOwner._id },
+                req.body,
+                { new: true }
+            );
+            if (!updatedHotel) return res.status(404).send('Hotel not found or not owned by you.');
+            res.send(updatedHotel);
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    },
+
+    deleteProperty: async (req, res) => {
+        try {
+            const hotel = await Hotel.findOneAndDelete({ _id: req.params.id, owner: req.hotelOwner._id });
+            if (!hotel) return res.status(404).send('Hotel not found or not owned by you.');
+            res.send({ message: 'Hotel successfully deleted' });
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    }
+};
+
+module.exports = HotelController;
--- HotelOwnerPanel/Routes/hotelRoutes.js
+++ HotelOwnerPanel/Routes/hotelRoutes.js
@@ -1,1 +1,22 @@
-console.log('Hello World!')
+const express = require('express');
+const { authenticateHotelOwner } = require('../Middleware/authMiddleware');
+const HotelController = require('../Controller/HotelController');
+
+const router = express.Router();
+
+// List properties owned by the hotel owner
+router.get('/', authenticateHotelOwner, HotelController.listProperties);
+
+// Get details of a single property
+router.get('/:id', authenticateHotelOwner, HotelController.getPropertyDetails);
+
+// Create a new property
+router.post('/', authenticateHotelOwner, HotelController.createProperty);
+
+// Update property details
+router.put('/:id', authenticateHotelOwner, HotelController.updatePropertyDetails);
+
+// Delete a property
+router.delete('/:id', authenticateHotelOwner, HotelController.deleteProperty);
+
+module.exports = router;
