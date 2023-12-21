6bdb1856d8af85c4cd828a86970b1f7c6b54cc10
huzaifa <mhuzaifaahmad@gmail.com>
2023-12-21 23:09:53 +0500

    Updated file paths in Hotel Owner Panel

--- HotelOwnerPanel/Middleware/authMiddleware.js
+++ HotelOwnerPanel/Middleware/authMiddleware.js
@@ -1,27 +1,30 @@
-const jwt = require('jsonwebtoken');
-const HotelOwner = require('D:/BS(SE)/Semester 5/Web Engineering/Project/TravelManagementBackend/Schemas/HotelOwnerProfile.schema'); 
+const jwt = require("jsonwebtoken");
+const HotelOwner = require("../../Schemas/HotelOwnerProfile.schema");
+//const HotelOwner = require("D:/BS(SE)/Semester 5/Web Engineering/Project/TravelManagementBackend/Schemas/HotelOwnerProfile.schema");
 const SECRET_KEY = process.env.SECRET_KEY; //for JWT (in .env file)
 
 const authenticateHotelOwner = async (req, res, next) => {
-    const token = req.cookies.auth_token; 
+  const token = req.cookies.auth_token;
 
-    if (!token) {
-        return res.status(401).send('Access Denied. No token provided.');
+  if (!token) {
+    return res.status(401).send("Access Denied. No token provided.");
+  }
+
+  try {
+    const verified = jwt.verify(token, SECRET_KEY);
+    const hotelOwner = await HotelOwner.findById(verified.id).select(
+      "-password"
+    );
+
+    if (!hotelOwner) {
+      throw new Error("The user no longer exists.");
     }
 
-    try {
-        const verified = jwt.verify(token, SECRET_KEY);
-        const hotelOwner = await HotelOwner.findById(verified.id).select('-password'); 
-        
-        if (!hotelOwner) {
-            throw new Error('The user no longer exists.');
-        }
-        
-        req.hotelOwner = hotelOwner; 
-        next();
-    } catch (err) {
-        res.status(400).send('Invalid token. Please log in again.');
-    }
+    req.hotelOwner = hotelOwner;
+    next();
+  } catch (err) {
+    res.status(400).send("Invalid token. Please log in again.");
+  }
 };
 
 module.exports = { authenticateHotelOwner };
