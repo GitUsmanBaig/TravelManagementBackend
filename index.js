95f7eab97f12866d2b0f8bb0d8872d22ad1c88a1
AbdullahDaniyal <abdullahdaniyal67@gmail.com>
2023-12-24 18:00:44 +0500

    Added cors permission to be accessed by the frorntend

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+}));
+
+
 
 // Routes Imports
 const TravelAgencyRouter = require("./TravelAgencyPanel/Routes/TravelAgencyRoutes");
