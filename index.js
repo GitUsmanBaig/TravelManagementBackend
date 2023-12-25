2b81a42253daaf562ebf91f47153fd3e1d271ad3
huzaifa <mhuzaifaahmad@gmail.com>
2023-12-25 19:41:40 +0500

    Add cloudinary image upload for travel agency logo

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-}));
-
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 
 // Routes Imports
 const TravelAgencyRouter = require("./TravelAgencyPanel/Routes/TravelAgencyRoutes");
